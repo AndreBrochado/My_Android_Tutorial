06c2146c56a36d1ce6e018349644295ef7e667f1
Andre <andrebrreis@gmail.com>
2016-04-19 23:35:27 +0100

    Initial Commit - Building Intent

---
+++ app/src/main/java/com/example/andre/mytestapp/MyActivity.java
@@ -0,0 +1,21 @@
+package com.example.andre.mytestapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    }
+}
